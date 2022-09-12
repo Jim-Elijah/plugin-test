42a4d672919a10a695ba08e2e3db36af3c4b9949
ustc-q <55303382+ustc-q@users.noreply.github.com>
2022-09-12 23:07:13 +0800

    '测试audio-player插件'

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <JimsAudioPlayer />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import JimsAudioPlayer from './components/jimsPlugin/JimsAudioPlayer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    JimsAudioPlayer,
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import AudioPlayer from 'jim-audio-player'
 
 Vue.config.productionTip = false
+Vue.use(AudioPlayer)
 
 new Vue({
   render: h => h(App),
